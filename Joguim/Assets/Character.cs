c2458273c5f9c18caf3c52ea3d59c28cf57d8c5d
Washington Mário de Jesus Souza <tonmjs@gmail.com>
2017-07-02 16:36:27 -0300

    no message

---
+++ Joguim/Assets/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public GameController GC;
+
+	// Use this for initialization
+	void Start () {
+		GC = GameObject.Find ("GameController").GetComponent<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GC.selectedChar == this) {
+			if (Input.GetKeyDown (KeyCode.LeftArrow))
+				transform.Translate (Vector2.left);
+
+			if (Input.GetKeyDown (KeyCode.RightArrow))
+				transform.Translate (Vector2.right);
+		}
+
+	}
+
+
+}
---
+++ Joguim/Assets/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameController : MonoBehaviour {
+
+	public Character char1;
+	public Character char2;
+	public Character[] currentChars;
+	public Character selectedChar;
+
+	// Use this for initialization
+	void Start () {
+
+		currentChars = new Character[2];
+
+		currentChars [0] = char1;
+		currentChars [1] = char2;
+
+		selectedChar = currentChars [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.Alpha1) || 
+			Input.GetKeyDown (KeyCode.Alpha2) || 
+			Input.GetKeyDown (KeyCode.Alpha3) || 
+			Input.GetKeyDown (KeyCode.Alpha4)) {
+
+			changeChar (detectPressedKey());
+		}
+		
+	}
+
+	void changeChar(KeyCode key){
+
+		selectedChar.gameObject.SetActive(false);
+
+		if (key == KeyCode.Alpha1)
+			selectedChar = currentChars [0];
+		else if(key == KeyCode.Alpha2)
+			selectedChar = currentChars [1];
+		else if(key == KeyCode.Alpha3)
+			selectedChar = currentChars [3];
+		else if(key == KeyCode.Alpha4)
+			selectedChar = currentChars [4];
+
+		selectedChar.gameObject.SetActive(true);
+	
+	}
+
+	public KeyCode detectPressedKey(){
+
+		KeyCode key = KeyCode.Alpha0;
+
+		foreach(KeyCode keyCode in Enum.GetValues(typeof(KeyCode))){
+			if (Input.GetKeyDown (keyCode))
+				key = keyCode;
+		}
+
+		return key;
+	}
+}
